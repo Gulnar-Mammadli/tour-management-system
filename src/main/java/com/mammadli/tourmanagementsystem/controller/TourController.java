21d034075377652a209aef522975f9e95d388e22
mammadli <gulnar.mammadli@ut.ee>
2023-04-19 18:25:21 +0300

    Add TourController

---
+++ src/main/java/com/mammadli/tourmanagementsystem/controller/TourController.java
@@ -0,0 +1,10 @@
+package com.mammadli.tourmanagementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/tour")
+public class TourController {
+
+}
