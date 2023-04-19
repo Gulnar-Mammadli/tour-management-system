919064468f81b93cd4c6b80bc8d3b850c1cbdf32
mammadli <gulnar.mammadli@ut.ee>
2023-04-19 18:25:03 +0300

    Add addUser implementation

---
+++ src/main/java/com/mammadli/tourmanagementsystem/service/UserService.java
@@ -0,0 +1,27 @@
+package com.mammadli.tourmanagementsystem.service;
+
+import com.mammadli.tourmanagementsystem.model.entity.User;
+import com.mammadli.tourmanagementsystem.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User addUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        log.info("A new user  {} is added", user.toString());
+        //return "A new user is added to system ";
+        return user;
+    }
+}
