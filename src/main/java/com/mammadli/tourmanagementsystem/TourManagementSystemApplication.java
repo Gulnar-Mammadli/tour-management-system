73739c0788682b7ad7c7291530d9a448a1da16e8
mammadli <gulnar.mammadli@ut.ee>
2023-04-21 17:13:40 +0300

    Add databaseInitialzer

--- src/main/java/com/mammadli/tourmanagementsystem/TourManagementSystemApplication.java
+++ src/main/java/com/mammadli/tourmanagementsystem/TourManagementSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TourManagementSystemApplication {
 
 	public static void main(String[] args) {
+		DatabaseInitializer.initialize("tour_db");
 		SpringApplication.run(TourManagementSystemApplication.class, args);
 	}
 
