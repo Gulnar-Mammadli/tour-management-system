ff8c20f364154db452dbaea90e8a6c7a9a1a7d3e
mammadli <gulnar.mammadli@ut.ee>
2023-04-21 17:11:39 +0300

    Update getAuthorities return type

--- src/main/java/com/mammadli/tourmanagementsystem/config/MyUserDetails.java
+++ src/main/java/com/mammadli/tourmanagementsystem/config/MyUserDetails.java
@@ -6,18 +6,18 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.List;
 
 public class MyUserDetails extends User implements UserDetails {
 
-    private User user;
+    private final User user;
 
     public MyUserDetails(final User user) {
         this.user = user;
     }
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
+    public List<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRoles());
         return Arrays.asList(authority);
     }
